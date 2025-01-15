d7a6810a06d5a2755b75255a4487fbacf2b13d42
thecabs <jaurescabreldongmo@gmail.com>
2025-01-15 23:30:34 +0100

    Rapport UT et TT pdf 4

--- resources/views/test-report.blade.php
+++ resources/views/test-report.blade.php
@@ -10,7 +10,7 @@
     </style>
 </head>
 <body>
-    <h1>Rapport des Tests Unitaires LARAVEL BLOG APP BACKEND</h1>
+    <h1>Rapport des Tests Unitaires Laravel BLOG APP BACKEND</h1>
     <p>Généré le : {{ now()->format('d/m/Y H:i:s') }}</p>
 
     <!-- Statistiques -->
