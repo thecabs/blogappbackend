5536999c791ca86ae6d5b99674e22bbfbbfd3977
thecabs <jaurescabreldongmo@gmail.com>
2025-01-16 00:44:49 +0100

    je teste l'envoi des mails

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -30,7 +30,10 @@
                     @endauth
                 </div>
             @endif
-
+            <div>
+    <h1>Bienvenue sur notre projet Laravel !</h1>
+    <p>Le projet fonctionne correctement et le pipeline CI/CD est également opérationnel pour le groupe 7.</p>
+</div>
             <div class="max-w-7xl mx-auto p-6 lg:p-8">
                 <div class="flex justify-center">
                     <svg viewBox="0 0 62 65" fill="none" xmlns="http://www.w3.org/2000/svg" class="h-16 w-auto bg-gray-100 dark:bg-gray-900">
