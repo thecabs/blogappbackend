841fbfbbbb84591a6ab06aa3c451a58463536847
thecabs <jaurescabreldongmo@gmail.com>
2025-01-17 07:18:50 +0100

    la prod

--- dbp-init.sql
+++ dbp-init.sql
@@ -1,4 +1,4 @@
-CREATE USER 'laravel'@'%' IDENTIFIED BY '';
+CREATE USER 'laravel_user'@'%' IDENTIFIED BY 'laravel_user';
 CREATE DATABASE IF NOT EXISTS laravel;
-GRANT ALL PRIVILEGES ON laravel.* TO 'root'@'%';
+GRANT ALL PRIVILEGES ON laravel.* TO 'laravel_user'@'%';
 FLUSH PRIVILEGES;
